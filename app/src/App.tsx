bf28df3abbbb697d138338e8be88f15b1e97e1df
Byenio <marcel.bienko@gmail.com>
2022-08-09 19:55:20 +0200

    🌱 Created function to check whether user's session is valid

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 import UserPosts from './Account/AccountNav/UserPosts';
 import NewPost from './Account/AccountNav/NewPost';
@@ -12,6 +13,34 @@ import Account from './Account';
 import Log from './Log';
 
 function App() {
+
+  async function loggedIn(): Promise<boolean> {
+
+    const cookies = new Cookies();
+
+    try {
+      const response = await fetch('http://localhost:1337/api/sessions', {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'x-refresh': cookies.get('refreshToken')
+        }),
+        body: ''
+      })
+
+      const result = await response.json();
+
+      console.log(result);
+
+      return response.ok;
+
+    } catch(ex) {
+      return false;
+    }
+
+  }
+
   return (
     <div className="App">
       
@@ -20,13 +49,13 @@ function App() {
         <Routes>
 
           <Route path='/' element={<Posts />}/>
-          <Route path='/posts' element={<Posts />}/>
-          <Route path='/account' element={
+          <Route path='/account' element = {
             <>
               <Account />
               <UserPosts />
             </>
-          }/>
+          } />
+
           <Route path='/log' element={<Log />}/>
 
           <Route path='/account/new-post' element={
